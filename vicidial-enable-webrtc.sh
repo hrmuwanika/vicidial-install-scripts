95cf143caabe90e8e89936b0775548040e4a11cd
Henry Robert Muwanika <hrmuwanika@gmail.com>
2023-09-27 00:53:46 +0200

    Add files via upload

--- vicidial-enable-webrtc.sh
+++ vicidial-enable-webrtc.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo "Download PBXWebPhone agc"
-cd /var/www/html/agc/
-git clone https://github.com/chornyitaras/PBXWebPhone.git
-
 echo "Install certbot for LetsEncrypt"
 if [ -f /etc/redhat-release ]; then
 	yum -y install certbot python2-certbot-apache mod_ssl
@@ -16,18 +12,18 @@ fi
 echo "Enter the DOMAIN NAME HERE. ***********IF YOU DONT HAVE ONE PLEASE DONT CONTINUE: "
 read DOMAINNAME
 
-wget -O /etc/httpd/conf.d/$DOMAINNAME.conf https://raw.githubusercontent.com/jaganthoutam/vicidial-install-scripts/main/DOMAINNAME.conf
+wget -O /etc/httpd/conf.d/$DOMAINNAME.conf https://github.com/GenXoutsourcing/vicidial-install-scripts/blob/main/DOMAINNAME.conf
 sed -i s/DOMAINNAME/"$DOMAINNAME"/g /etc/httpd/conf.d/$DOMAINNAME.conf
 
 echo "Please Enter EMAIL and Agree the Terms and Conditions "
 certbot --apache -d $DOMAINNAME
 
 echo "Change http.conf in Asterisk"
-wget -O /etc/asterisk/http.conf https://raw.githubusercontent.com/jaganthoutam/vicidial-install-scripts/main/asterisk-http.conf
+wget -O /etc/asterisk/http.conf https://github.com/GenXoutsourcing/vicidial-install-scripts/main/asterisk-http.conf
 sed -i s/DOMAINNAME/"$DOMAINNAME"/g /etc/asterisk/http.conf
 
 echo "Change sip.conf in Asterisk"
-wget -O /etc/asterisk/sip.conf https://raw.githubusercontent.com/jaganthoutam/vicidial-install-scripts/main/asterisk-sip.conf
+wget -O /etc/asterisk/sip.conf https://github.com/GenXoutsourcing/vicidial-install-scripts/main/asterisk-sip.conf
 sed -i s/DOMAINNAME/"$DOMAINNAME"/g /etc/asterisk/sip.conf
 
 echo "Reloading Asterisk"
