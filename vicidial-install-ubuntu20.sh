f9910ab8ad8f505257dc43094d293573b4eae064
Henry Robert Muwanika <hrmuwanika@gmail.com>
2023-10-09 15:34:34 +0200

    Update vicidial-install-ubuntu20.sh

--- vicidial-install-ubuntu20.sh
+++ vicidial-install-ubuntu20.sh
@@ -38,13 +38,13 @@ sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] https://mariadb.mirror.l
 sudo apt update && sudo apt -y upgrade
 sudo apt install apache2 apache2-bin apache2-data apache2-utils mariadb-server mariadb-client mariadb-backup php7.4 libapache2-mod-php7.4 php7.4-common php7.4-sqlite3 php7.4-json php7.4-curl \
  php7.4-intl php7.4-mbstring php7.4-xmlrpc php7.4-mysql php7.4-ldap php7.4-gd php7.4-xml php7.4-cli php7.4-zip php7.4-soap php7.4-imap php7.4-bcmath wget unzip curl \
- git libssl-dev libmysqlclient-dev sox sipsak lame screen -y
+ git libssl-dev libmysqlclient-dev sox sipsak lame screen libploticus0-dev libsox-fmt-all mpg123 mytop ploticus  -y
 
 sudo a2enmod dav
 sudo a2enmod dav_svn
  
 sudo apt install php7.4-opcache  php7.4-dev php7.4-readline sox lame screen libnet-telnet-perl libasterisk-agi-perl libelf-dev autogen libtool shtool libdbd-mysql-perl \
- libsrtp2-dev uuid-dev unzip libjansson-dev sqlite3 libxml2-dev libncurses5-dev libsqlite3-dev libedit-dev libnewt-dev htop sngrep -y
+ libsrtp2-dev uuid-dev unzip libjansson-dev sqlite3 libxml2-dev libncurses5-dev libsqlite3-dev libedit-dev libnewt-dev htop sngrep libcurl3 -y
 
 sudo systemctl enable apache2.service
 sudo systemctl start apache2.service
@@ -133,7 +133,9 @@ apt install libdbd-mysql-perl -y
 
 
 read -p 'Press Enter to continue And Install Dahdi: '
-#Install dahdi
+#--------------------------------------------------
+# Install dahdi
+#--------------------------------------------------
 apt install dahdi-* dahdi -y
 modprobe dahdi
 modprobe dahdi_dummy
@@ -141,7 +143,10 @@ modprobe dahdi_dummy
 
 read -p 'Press Enter to continue And Install LibPRI and Asterisk: '
 
-#Install Asterisk 
+#--------------------------------------------------
+# Install Asterisk core
+#--------------------------------------------------
+
 mkdir /usr/src/asterisk
 cd /usr/src/asterisk
 wget http://download.vicidial.com/required-apps/asterisk-13.29.2-vici.tar.gz  
@@ -167,7 +172,16 @@ systemctl start asterisk
 read -p 'Press Enter to continue: '
 echo 'Continuing...'
 
-#Install astguiclient
+#--------------------------------------------------
+# Install astguiclient
+#--------------------------------------------------
+
+# Install Perl Asterisk Extension
+cd /usr/src
+tar -xf asterisk-perl-0.08.tar.gz
+cd asterisk-perl-0.08/
+perl Makefile.PL && make all && make install
+
 echo "Installing astguiclient"
 mkdir /usr/src/astguiclient
 cd /usr/src/astguiclient
@@ -190,9 +204,11 @@ GRANT RELOAD ON *.* TO cron@localhost;
 GRANT RELOAD ON *.* TO custom@'%';
 GRANT RELOAD ON *.* TO custom@localhost;
 flush privileges;
+SET GLOBAL connect_timeout=60;
 use asterisk;
 \. /usr/src/astguiclient/trunk/extras/MySQL_AST_CREATE_tables.sql
 \. /usr/src/astguiclient/trunk/extras/first_server_install.sql
+\. /usr/src/astguiclient/trunk/extras/sip-iax_phones.sql
 update servers set asterisk_version='13.29.2';
 quit
 MYSQL_SCRIPT
