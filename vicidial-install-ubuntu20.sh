e81492bf45c959ebc255e3dd44ff588ee7fe643d
Henry Robert Muwanika <hrmuwanika@gmail.com>
2023-10-11 07:40:52 +0200

    Update vicidial-install-ubuntu20.sh

--- vicidial-install-ubuntu20.sh
+++ vicidial-install-ubuntu20.sh
@@ -158,10 +158,10 @@ read -p 'Press Enter to continue And Install LibPRI and Asterisk: '
 #--------------------------------------------------
 sudo mkdir /usr/src/asterisk
 cd /usr/src/asterisk
-wget http://download.vicidial.com/beta-apps/asterisk-16.17.0-vici.tar.gz
-tar -xvzf asterisk-16.17.0-vici.tar.gz
-cd asterisk-16*
-sudo ./configure --with-pjproject-bundled
+wget http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-18.19.0.tar.gz
+tar -xvzf asterisk-18.19.0.tar.gz
+cd asterisk-18*
+sudo ./configure --with-gsm=internal --enable-opus --enable-srtp --with-ssl --enable-asteriskssl --with-pjproject-bundled --with-jansson-bundled
 sudo make clean
 sudo make menuselect    
 sudo make 
@@ -217,7 +217,7 @@ flush privileges;
 use asterisk;
 \. /usr/src/astguiclient/trunk/extras/MySQL_AST_CREATE_tables.sql
 \. /usr/src/astguiclient/trunk/extras/first_server_install.sql
-update servers set asterisk_version='20.4.0';
+update servers set asterisk_version='18.19.0';
 quit
 MYSQL_SCRIPT
 
