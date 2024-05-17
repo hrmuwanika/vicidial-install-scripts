d1b2d0e0f55e01843a06ae4ff54b3c41ab5a00c5
Henry Robert Muwanika <hrmuwanika@gmail.com>
2024-05-17 20:49:31 +0200

    Update confbridges.sh

--- confbridges.sh
+++ confbridges.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
-echo "Upgrade Asterisk 18 to use ConfBridges"
+echo "Upgrade Asterisk 20 to use ConfBridges"
 
 cd /usr/src/
 rm -rf vicidial-install-scripts
 git clone https://github.com/hrmuwanika/vicidial-install-scripts.git
-
 cd /usr/src/vicidial-install-scripts/
+
 yes | cp -rf extensions.conf /etc/asterisk/extensions.conf
 mv confbridge-vicidial.conf /etc/asterisk/
+
+tee -a /etc/asterisk/pjsip.conf <<EOF
+
+#include "pjsip_wizard-vicidial.conf"
+EOF
 
 tee -a /etc/asterisk/confbridge.conf <<EOF
 
@@ -26,8 +31,16 @@ eventfilter=Event: Meetme
 eventfilter=Event: Confbridge
 EOF
 
+tee -a /etc/asterisk/modules.conf <<EOF
+
+load = res_timing_dahdi.so
+
+noload = res_timing_timerfd.so
+noload = res_timing_kqueue.so
+noload = res_timing_pthread.so
+EOF
 
 sed -i 's|vicidial_conferences|vicidial_confbridges|g' /var/www/html/vicidial/non_agent_api.php
 
-
+systemctl restart asterisk.service
 
