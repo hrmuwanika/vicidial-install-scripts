8fe9a4dbe9f1b0836014a178927ccdefb9e48230
Henry Robert Muwanika <hrmuwanika@gmail.com>
2024-05-22 11:22:55 +0200

    debian12_ast20.sh

--- vicidial-install-debian12.sh
+++ vicidial-install-debian12.sh
@@ -427,7 +427,7 @@ tee -a ~/.bashrc <<EOF
 
 # Commands
 /usr/share/astguiclient/ADMIN_keepalive_ALL.pl --cu3way
-/usr/share/astguiclient/AST_VDhopper.pl
+/usr/share/astguiclient/AST_VDhopper.pl -q
 EOF
 
 chmod -R 777 /var/spool/asterisk/monitorDONE
