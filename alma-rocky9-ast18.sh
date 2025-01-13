acc26dcc49a72a8c1cec937cf48079c625f83812
Henry Robert Muwanika <hrmuwanika@gmail.com>
2025-01-13 19:02:11 +0200

    Update alma-rocky9-ast18.sh

--- alma-rocky9-ast18.sh
+++ alma-rocky9-ast18.sh
@@ -334,39 +334,7 @@ ldconfig
 
 # Install Dahdi
 echo "Install Dahdi"
-ln -sf /usr/lib/modules/$(uname -r)/vmlinux.xz /boot/
-cd /etc/include
-wget https://dialer.one/newt.h
-
-cd /usr/src/
-## wget https://downloads.asterisk.org/pub/telephony/dahdi-linux-complete/dahdi-linux-complete-3.4.0+3.4.0.tar.gz
-mkdir dahdi-linux-complete-3.4.0+3.4.0
-## tar -xzvf dahdi-linux-complete-3.4.0+3.4.0.tar.gz
-cd dahdi-linux-complete-3.4.0+3.4.0
-wget https://cybur-dial.com/dahdi-9.4-fix.zip
-unzip dahdi-9.4-fix.zip
-yum -y install newt* 
-
-## sudo sed -i 's|(netdev, \&wc->napi, \&wctc4xxp_poll, 64);|(netdev, \&wc->napi, \&wctc4xxp_poll);|g' /usr/src/dahdi-linux-complete-3.4.0+3.4.0/linux/drivers/dahdi/wctc4xxp/base.c
-## sudo sed -i 's|<linux/pci-aspm.h>|<linux/pci.h>|g' /usr/src/dahdi-linux-complete-3.2.0+3.2.0/linux/include/dahdi/kernel.h
-
-make clean
-make
-make install
-make install-config
-
-cd tools
-make clean
-make
-make install
-make install-config
-
-cp /etc/dahdi/system.conf.sample /etc/dahdi/system.conf
-
-echo "Install Dahdi package"
-yum -y install kernel-devel kernel-devel-$(uname -r)
-
-yum -y install dahdi* asterisk-dahdi dahdi-tools-libs
+cd /usr/src && wget https://docs.phreaknet.org/script/phreaknet.sh && chmod +x phreaknet.sh && ./phreaknet.sh dahdi
 modprobe dahdi
 modprobe dahdi_dummy
 /usr/sbin/dahdi_cfg -vvvvvvvvvv
