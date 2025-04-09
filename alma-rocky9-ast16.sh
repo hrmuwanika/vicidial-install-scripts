aad1e482ecf6f5195b22344c6c7e040e659913bd
Henry Robert Muwanika <hrmuwanika@gmail.com>
2025-04-09 17:52:28 +0200

    Update alma-rocky9-ast16.sh

--- alma-rocky9-ast16.sh
+++ alma-rocky9-ast16.sh
@@ -894,7 +894,7 @@ perl install.pl --no-prompt
 sudo cat <<CRONTAB > /root/crontab-file
 
 ## Asterisk start fix
-@reboot sleep 10; /usr/share/astguiclient/start_asterisk_boot.pl
+@reboot sleep 15 && /usr/share/astguiclient/start_asterisk_boot.pl
 
 ### Audio Sync hourly
 * 1 * * * /usr/share/astguiclient/ADMIN_audio_store_sync.pl --upload --quiet
