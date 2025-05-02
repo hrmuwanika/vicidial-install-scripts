79161ed9f03063ae6287ac967cf5931aeae9507d
Henry Robert Muwanika <hrmuwanika@gmail.com>
2025-05-02 16:42:44 +0200

    Update ubuntu-ast18.sh

--- ubuntu-ast18.sh
+++ ubuntu-ast18.sh
@@ -410,6 +410,9 @@ wget http://download.vicidial.com/required-apps/asterisk-18.21.0-vici.tar.gz
 tar -xvzf asterisk-18.21.0-vici.tar.gz
 tar -xvzf libpri-1.6.1.tar.gz
 
+rm -f asterisk-18.21.0-vici.tar.gz
+rm -f libpri-1.6.1.tar.gz
+
 cd /usr/src/asterisk-18.21.0-vici
 ./contrib/scripts/install_prereq install
 ./contrib/scripts/get_mp3_source.sh
